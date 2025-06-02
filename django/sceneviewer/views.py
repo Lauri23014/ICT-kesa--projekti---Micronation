5f671da56171568303757c87c29c196d0f60201d
Lunahere <at.lunahere@gmail.com>
2025-06-02 09:40:44 +0300

    changed 360 image viewer from marzipano to photo sphere viewer

--- django/sceneviewer/views.py
+++ django/sceneviewer/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 
 def index(request):
-	return render(request, "sceneviewer/marzipano/app-files/index.html")
+	return render(request, "sceneviewer/index.html")
+	#return render(request, "sceneviewer/marzipano/app-files/index.html")
     #return HttpResponse("Hello, world. You're at the sceneviewer index.")	#simple hello world
