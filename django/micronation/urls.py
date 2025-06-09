fe278a11a7fe15d70c6656b38ab1e3b6c1bc0e76
NittaJ <nitta21000@student.hamk.fi>
2025-06-09 12:29:51 +0300

    User profile and user management pages created

--- django/micronation/urls.py
+++ django/micronation/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
 	path("view/", include("sceneviewer.urls")),
     path("admin/", admin.site.urls),
+    path("user/", include("users.urls"))
 ]
