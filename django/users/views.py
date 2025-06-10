1921c5f722159e4ae6e5e518559f04a0f70de79b
NittaJ <nitta21000@student.hamk.fi>
2025-06-10 10:43:14 +0300

    fiksaus

--- django/users/views.py
+++ django/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-def user_profile(request, user_id):
-    return render(request, "users/profile.html", {"user_id": user_id})
+def index(request):
+    return HttpResponse("Käyttäjien etusivu.")
