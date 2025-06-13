47f9959cf476dfe2c24fa5377dd7b1093ae79fdf
Lunahere <at.lunahere@gmail.com>
2025-06-13 22:57:58 +0300

    moved upload functionality to admin interface

--- django/sceneviewer/urls.py
+++ django/sceneviewer/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from django.conf import settings
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-	path("upload", views.upload_scene, name="upload")
+	#path("upload", views.upload_scene, name="upload") #!!!WARNING!!! test page for uploading scenes without admin interface, do not use in production
 ]
