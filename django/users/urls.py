53e7c0fdc45b3d2638338a19abac4b38efb6f029
NittaJ <nitta21000@student.hamk.fi>
2025-06-15 17:13:49 +0300

    user login and logout

--- django/users/urls.py
+++ django/users/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 urlpatterns = [
     path("", views.profile, name="profile"),
-    path('register/', views.register_view, name ="register")
+    path('register/', views.register_view, name ="register"),
+    path('login/', views.login_view, name ="login"),
+    path('logout/', views.logout_view, name ="logout"),
+    
+    
 ]
--- django/users/views.py
+++ django/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect 
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 def profile(request):
     return render(request, 'users/profile.html', {})
@@ -8,9 +9,24 @@ def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid(): #form validation
-            form.save()
+            login(request, form.save())
             return redirect("index")  #can be changed later to redirect to home page 
     else:
         form = UserCreationForm()  
 
     return render(request, "users/register.html", {"form": form})  
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect("index")
+    else:
+        form = AuthenticationForm()
+    return render(request, "users/login.html", {"form": form})     
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("index") #should be later changed to another page
