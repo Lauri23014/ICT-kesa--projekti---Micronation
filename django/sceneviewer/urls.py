a5c9dc8ac339f1e3656d03d401bcd4ab803c636f
Lunahere <at.lunahere@gmail.com>
2025-06-15 21:01:18 +0300

    cleaned up branch ahead of merge

--- django/sceneviewer/urls.py
+++ django/sceneviewer/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-	#path("upload", views.upload_scene, name="upload") #!!!WARNING!!! test page for uploading scenes without admin interface, do not use in production
 ]
