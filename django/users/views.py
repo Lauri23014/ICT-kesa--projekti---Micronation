9dcde0b67241fa4161e3eb852c71059b7c9974b5
NittaJ <nitta21000@student.hamk.fi>
2025-06-15 15:23:50 +0300

    registeration form validation and aria labels added

--- django/users/views.py
+++ django/users/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from django.contrib.auth.forms import UserCreationForm
 
 def profile(request):
     return render(request, 'users/profile.html', {})
 
 def register_view(request):
-    form = UserCreationForm()
-    return render(request, "users/register.html", {"form": form})
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid(): #form validation
+            form.save()
+            return redirect("index")  #can be changed later to redirect to home page 
+    else:
+        form = UserCreationForm()  
+
+    return render(request, "users/register.html", {"form": form})  
