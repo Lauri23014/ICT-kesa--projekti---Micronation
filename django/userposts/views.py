db1af92cdb70ddc33c25a0db2969043d57236ef2
Lunahere <at.lunahere@gmail.com>
2025-06-29 17:06:10 +0300

    plan for view

--- django/userposts/views.py
+++ django/userposts/views.py
@@ -3,6 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
+# TODO: page for posts
+# django tutorial for templates (look "include")
+# | https://docs.djangoproject.com/en/5.2/ref/templates/builtins/
+# format:
+# (post-mini1) 	op
+# (post-mini2) 	replying to op
+# post			replying to post-mini2
+# (post-mini3) 	reply to post
 def post(request, username, post):
 	return HttpResponse(username+" - "+post)
 	#return render(request, "userposts/post.html")
