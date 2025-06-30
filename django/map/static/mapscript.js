bdaa2577f69fdb3090bfe735542a212fce3f0a6a
Lauri23014 <lerajala@gmail.com>
2025-06-30 14:10:18 +0300

    minor tweaks to thumbnails

--- django/map/static/mapscript.js
+++ django/map/static/mapscript.js
@@ -9,7 +9,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 //marker
 var marker = L.marker([51.5, -0.09]).addTo(map);
-marker.bindPopup('<b>Hello world!</b><br><img src="/static/testthumbnail.jpg" alt="thumbnail" style="height:125px;width:auto;display:block;margin-left:auto;margin-right:auto;"><br>I am a popup. Click me twice to view a scene.').openPopup();
+marker.bindPopup('<b>Hello world!</b><br><img src="/static/testthumbnail.jpg" alt="thumbnail" style="height:125px;width:auto;max-width:220px;display:block;margin-left:auto;margin-right:auto;padding:5px;"><br>I am a popup. Click me twice to view a scene.').openPopup();
 
 function OnMarkerClick() {
     window.open("/view");
