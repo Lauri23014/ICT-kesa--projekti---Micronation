829075ea10bf295253ffb12c359dfa799b7189a1
Lunahere <at.lunahere@gmail.com>
2025-07-05 23:30:54 +0300

    added likes to admin view

--- django/userposts/admin.py
+++ django/userposts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Like
 
 #admin.site.register(Post)
 @admin.register(Post)
@@ -10,3 +10,5 @@ class PostAdmin(admin.ModelAdmin):
 	def save_model(self, request, obj, form, change):
 		obj.user = request.user
 		super().save_model(request, obj, form, change)
+
+admin.site.register(Like)
