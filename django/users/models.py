b7ce0abf6eb17a1cf39b37e0f3974f54ced46e46
NittaJ <nitta21000@student.hamk.fi>
2025-07-06 23:15:52 +0300

    stylin

--- django/users/models.py
+++ django/users/models.py
@@ -8,6 +8,9 @@ class Profile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)  #on_dele deletes user profile automatically when the user is deleted
     bio = models.TextField()
     profile_pic = models.ImageField(null=True, blank=True, upload_to="profile/")
+    theme_color = models.CharField(max_length=7, default='#1a1a1a')
+    favorite_cat = models.CharField(max_length=100, blank=True, null=True)
+
 
 
     def __str__(self):
