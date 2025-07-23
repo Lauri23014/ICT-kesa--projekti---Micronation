ef50b447e3bd8145d105177e9fc66beccb70b866
Lunahere <at.lunahere@gmail.com>
2025-07-23 13:59:49 +0300

    added dynamically created map pins based on scene model

--- django/map/views.py
+++ django/map/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from sceneviewer.models import Scene
 
 # Create your views here.
 def index(request):
-	return render(request, "map/index.html")
+	context = {
+		"scenes" : Scene.objects.all()
+	}
+	return render(request, "map/index.html", context=context)
