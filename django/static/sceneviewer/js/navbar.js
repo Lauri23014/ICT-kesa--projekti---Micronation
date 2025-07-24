b44538f3ba7bbfc8fce62d70a4d4e5008af70792
NittaJ <nitta21000@student.hamk.fi>
2025-07-24 17:30:25 +0300

    navbar stylic change

---
+++ django/static/sceneviewer/js/navbar.js
@@ -0,0 +1,5 @@
+document.getElementById("paw-trigger").onclick = function () {
+  document.querySelectorAll("#navlinks ul li").forEach(function (li) {
+    li.classList.toggle("visible");
+  });
+};
