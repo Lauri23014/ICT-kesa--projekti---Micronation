c8f0560728e49c070db7f0bf9af36ab80967a716
NittaJ <nitta21000@student.hamk.fi>
2025-07-26 20:17:33 +0300

    added svg to homebagebanner

--- django/static/sceneviewer/js/navbar.js
+++ django/static/sceneviewer/js/navbar.js
@@ -1,5 +1,5 @@
-document.getElementById("paw-trigger").onclick = function () {
-  document.querySelectorAll("#navlinks ul li").forEach(function (li) {
-    li.classList.toggle("visible");
-  });
-};
+  document.getElementById("paw-trigger").onclick = function () {
+    document.querySelectorAll("#navlinks ul li").forEach(function (li) {
+      li.classList.toggle("visible");
+    });
+  };
