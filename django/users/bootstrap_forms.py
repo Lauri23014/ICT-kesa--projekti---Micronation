7fd9dc232e211c16a5bc172e8522a2f3781d19c6
Lunahere <at.lunahere@gmail.com>
2025-07-29 18:49:14 +0300

    fixed a bug with form icons, made universal button class

--- django/users/bootstrap_forms.py
+++ django/users/bootstrap_forms.py
@@ -6,30 +6,25 @@ from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
 class EditProfileForm(forms.ModelForm):
     username = forms.CharField(
         max_length=100,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={'class': 'form-control profile-form-field'})
     )
 
     class Meta:
         model = User
         fields = ('username',)
 
-   
-        
-
-
-
 class PasswordChangingForm(PasswordChangeForm):
     old_password = forms.CharField(
         label="Old password",
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+        widget=forms.PasswordInput(attrs={'class': 'form-control profile-form-field'})
     )
     new_password1 = forms.CharField(
         label="New password",
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+        widget=forms.PasswordInput(attrs={'class': 'form-control profile-form-field'})
     )
     new_password2 = forms.CharField(
         label="Confirm new password",
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+        widget=forms.PasswordInput(attrs={'class': 'form-control profile-form-field'})
     )
 
     class Meta:
@@ -40,16 +35,13 @@ class ProfileEditForm(forms.ModelForm):
     bio = forms.CharField(
         required=False,
         widget=forms.Textarea(attrs={
-            'class': 'form-control',
-           
+            'class': 'form-control profile-form-field',
         })
     )
 
     class Meta:
         model = Profile
         fields = ['bio', 'profile_pic']
-
-
 
 class BackgroundImageForm(forms.ModelForm):
     class Meta:
